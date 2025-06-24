2379d521cc57b046e0ed869840dd919ea82c22ee
Habibullo22 <habibulloabidjonov555@gmail.com>
2025-06-24 12:14:09 +0500

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot tirik!"
+
+def keep_alive():
+    t = Thread(target=lambda: app.run(host='0.0.0.0', port=8080))
+    t.start()
+
+if __name__ == "__main__":
+    keep_alive()
---
+++ main.py
@@ -0,0 +1,1 @@
+<cleaned main.py code here>
